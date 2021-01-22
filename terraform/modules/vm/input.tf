308182245438571a69ee05dfafc6f152e96be1b8
makrisramasamy <46874201+makrisramasamy@users.noreply.github.com>
2021-01-21 23:58:55 -0500

    Update input.tf

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,36 +1,9 @@
-resource "azurerm_network_interface" "test" {
-  name                = "nic123"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+variable "location" {}
+variable "resource_group" {}
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = "${var.subnet_id}"
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${var.public_ip_address_id}"
-  }
-}
 
-resource "azurerm_linux_virtual_machine" "test" {
-  name                = "mynewvm"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
-  size                = "Standard_B1ls"
-  admin_username      = "mani"
-  network_interface_ids = ["${azurerm_network_interface.test.id}"]
-  admin_ssh_key {
-    username   = "mani"
-    public_key = file("~/keyfile.pub")
-  }
-  os_disk {
-    caching           = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-}
+variable "application_type" {}
+variable "resource_type" {}
 
+variable "subnet_id" {}
+variable "public_ip_address_id" {}

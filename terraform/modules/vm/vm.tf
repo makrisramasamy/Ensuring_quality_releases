0e71f3cb2e9d024dd6b85cb0f3588d68afd7dee3
makrisramasamy <46874201+makrisramasamy@users.noreply.github.com>
2021-01-27 22:11:23 -0500

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   network_interface_ids = ["${azurerm_network_interface.test.id}"]
   admin_ssh_key {
     username   = "mani"
-    public_key = file("~/.ssh/udacity_azure")
+    public_key = file("~/.ssh/udacity_azure/id_rsa.pub")
   }
   os_disk {
     caching           = "ReadWrite"
